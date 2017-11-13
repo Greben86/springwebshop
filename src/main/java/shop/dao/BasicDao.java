72ff05db2db6a3471b4ed71d93d75f4df2843d8e
Victor Greben <grebenvictor@yandex.ru>
2017-11-13 17:56:39 +0300

    Add comments

--- src/main/java/shop/dao/BasicDao.java
+++ src/main/java/shop/dao/BasicDao.java
@@ -3,14 +3,33 @@ package shop.dao;
 import java.util.List;
 
 public interface BasicDao<T> {
-
+    /**
+     * Get list of entities
+     * @param filter - sql-filter of list
+     */
     List<T> getList(String filter);
 
-    String updateList(List<T> list);
-
+    /**
+     * Find entity by id
+     * @param id - id of entity
+     */
     T findById(Long id);
 
+    /**
+     * Update list entities
+     * @param list - list entities
+     */
+    String updateList(List<T> list);
+
+    /**
+     * Update or insert entity
+     * @param enetity - entity
+     */
     T updateOrInsert(T entity);
 
+    /**
+     * Delete entity
+     * @param entity - entity
+     */
     T delete(T entity);
 }
--- src/main/java/shop/model/ImageControl.java
+++ src/main/java/shop/model/ImageControl.java
@@ -5,9 +5,22 @@ import java.io.InputStream;
 
 public interface ImageControl {
 
+    /**
+     * Save image into hard-disk
+     * @param good - entity good
+     * @param stream - file input stream
+     */
     String saveFile(Good good, InputStream stream);
 
+    /**
+     * Read image for hard-disk
+     * @param good - entity good
+     */
     byte[] readFile(Good good);
 
+    /**
+     * Remove image for har-disk
+     * @param good - entity good
+     */
     String removeFile(Good good);
 }
