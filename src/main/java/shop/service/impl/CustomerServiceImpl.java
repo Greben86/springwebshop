cc89f967b4f10ff8c9834b892ebad2bfab7467d7
VictorGreben <grebenvictor@yandex.ru>
2018-03-20 15:47:16 +0300

    Lucene search begun! Add lucene version of 7.2.1 Fix search module Перевел поиск на api Bug fix from search A little change search Add user data access object Add Add recpacha2 into register module A little change
    Merge origin/development/inwork into development/inwork
    
    Conflicts:
    	application/layouts/master.phtml
    	application/modules/payment/views/scripts/index/index.phtml

--- src/main/java/shop/service/impl/CustomerServiceImpl.java
+++ src/main/java/shop/service/impl/CustomerServiceImpl.java
@@ -3,55 +3,55 @@ package shop.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import shop.service.CustomerService;
 import shop.entity.Customer;
 import shop.dao.CustomerDao;
 
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
-	@Autowired
-	private CustomerDao customerDao;
 
-	@Override
-	public List<Customer> getList() {
-		return customerDao.getList("");
-	}
+    @Autowired
+    private CustomerDao customerDao;
 
-	@Override
-	public Boolean updateOrInsert(Customer customer) {
-		customerDao.updateOrInsert(customer);
-		return true;
-	}
-
-	@Override
-	public Boolean updateList(List<Customer> list) {
-		customerDao.updateList(list);		
-		return true;
-	}
-
-	@Override
-	public Boolean delete(Customer customer) {
-		if (customer!=null) {
-			customerDao.delete(customer);
-			return true;
-		} else {
-			return false;
-		}		
+    @Override
+    public List<Customer> getList() {
+        return customerDao.getList("");
     }
-    
+
+    @Override
+    public Boolean updateOrInsert(Customer customer) {
+        customerDao.updateOrInsert(customer);
+        return true;
+    }
+
+    @Override
+    public Boolean updateList(List<Customer> list) {
+        customerDao.updateList(list);
+        return true;
+    }
+
+    @Override
+    public Boolean delete(Customer customer) {
+        if (customer != null) {
+            customerDao.delete(customer);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public Boolean checkPass(String number, String pass) {
         Customer entity = customerDao.findCustomerByNumber(number);
-		if (entity!=null) {
-			return pass.equalsIgnoreCase(entity.getPass());
-		} else {
-			return false;
-		}
-	}
+        if (entity != null) {
+            return pass.equalsIgnoreCase(entity.getPass());
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public Customer getById(Long id) {
-		return customerDao.findById(id);
-	}
+    @Override
+    public Customer getById(Long id) {
+        return customerDao.findById(id);
+    }
 }
--- src/main/java/shop/service/impl/GoodServiceImpl.java
+++ src/main/java/shop/service/impl/GoodServiceImpl.java
@@ -6,87 +6,87 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import shop.service.GoodService;
 import shop.entity.Good;
-import shop.dao.GoodDao; 
-import org.apache.log4j.Logger;
+import shop.dao.GoodDao;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 
 @Service("goodService")
 public class GoodServiceImpl implements GoodService {
-	private static final Logger LOG = Logger.getLogger(GoodServiceImpl.class);
-	@Autowired
-	private GoodDao goodDao;
 
-        @CacheEvict(value="folders", key="#good.id")
-	@Override
-	public Boolean updateOrInsert(Good good) {
-		goodDao.updateOrInsert(good);
-		return true;
-	}
+    @Autowired
+    private GoodDao goodDao;
 
-        @CacheEvict(value="folders", allEntries=true)
-	@Override
-	public Boolean updateList(List<Good> list) {
-		goodDao.updateList(list);		
-		return true;
-	}
+    @CacheEvict(value = "folders", key = "#good.id")
+    @Override
+    public Boolean updateOrInsert(Good good) {
+        goodDao.updateOrInsert(good);
+        return true;
+    }
 
-        @CacheEvict(value="folders", key="#good.id")
-	@Override
-	public Boolean delete(Good good) {
-		if (good!=null) {
-			goodDao.delete(good);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-        @Cacheable("folders")
-	@Override
-	public List<Good> getFolders(Long owner) {
-		List<Good> list = goodDao.getList("(`FOLDER`='T')" + ((owner>=0) ? " AND (`OWNER`="+owner+")" : ""));
-		checkFolders(list);
-		return list;
-	}
+    @CacheEvict(value = "folders", allEntries = true)
+    @Override
+    public Boolean updateList(List<Good> list) {
+        goodDao.updateList(list);
+        return true;
+    }
 
-	@Override
-	public List<Good> getList(Long owner) {
-		List<Good> list = goodDao.getList((owner>=0) ? "(`OWNER`="+owner+")" : "");
-                if (list!=null)
-                    checkFolders(list);
-		return list;
-	}
+    @CacheEvict(value = "folders", key = "#good.id")
+    @Override
+    public Boolean delete(Good good) {
+        if (good != null) {
+            goodDao.delete(good);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	private void checkFolders(List<Good> list) {
-		List<Good> source = goodDao.getList("");
-		Iterator<Good> iterator = list.iterator();
-		while (iterator.hasNext()) {
-			Good good = iterator.next();
-			if (good.getFolder()) {
-				if (calcChild(good, source)==0) {
-					iterator.remove();
-				}
-			}
-		}
-	}
+    @Cacheable("folders")
+    @Override
+    public List<Good> getFolders(Long owner) {
+        List<Good> list = goodDao.getList("(`FOLDER`='T')" + ((owner >= 0) ? " AND (`OWNER`=" + owner + ")" : ""));
+        checkFolders(list);
+        return list;
+    }
 
-	private Integer calcChild(Good owner, List<Good> list) {
-		for (Good good : list) {
-			if (good.getOwner().equals(owner.getId())) {
-				if (good.getFolder()) {
-					owner.setHaschild(true);
-					owner.setChildcount(owner.getChildcount() + calcChild(good, list));
-				} else {
-					owner.setChildcount(owner.getChildcount() + 1);
-				}				
-			}
-		}
+    @Override
+    public List<Good> getList(Long owner) {
+        List<Good> list = goodDao.getList((owner >= 0) ? "(`OWNER`=" + owner + ")" : "");
+        if (list != null) {
+            checkFolders(list);
+        }
+        return list;
+    }
+
+    private void checkFolders(List<Good> list) {
+        List<Good> source = goodDao.getList("");
+        Iterator<Good> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Good good = iterator.next();
+            if (good.getFolder()) {
+                if (calcChild(good, source) == 0) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    private Integer calcChild(Good owner, List<Good> list) {
+        list.stream()
+                .filter((Good good) -> good.getOwner().equals(owner.getId()))
+                .forEach((Good good) -> {
+                    if (good.getFolder()) {
+                        owner.setHaschild(true);
+                        owner.setChildcount(owner.getChildcount() + calcChild(good, list));
+                    } else {
+                        owner.setChildcount(owner.getChildcount() + 1);
+                    }
+                });
         return owner.getChildcount();
-	}
+    }
 
-	@Override
-	public Good getById(Long id) {
-		return goodDao.findById(id);
-	}
+    @Override
+    public Good getById(Long id) {
+        return goodDao.findById(id);
+    }
 }
